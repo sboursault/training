8a5ac678d1bb30982a06d498629549d686fcabaa
sboursault <sboursault@protonmail.com>
2024-11-17 18:13:46 +0100

    pw version for 'lets practice' part

--- presentation/src/js/html-processor.js
+++ presentation/src/js/html-processor.js
@@ -14,40 +14,48 @@ export function leftPadCode(html) {
 }
 
 export function removeFalsyIfs(html, e2eTool) {
-  const regEx =
-    e2eTool === 'pw'
-      ? /\s*<if-cy(?:\s+[\w-]+="[^"]*")*>[\s\S]*?<\/if-cy>/g
-      : /\s*<if-pw(?:\s+[\w-]+="[^"]*")*>[\s\S]*?<\/if-pw>/g
-  return html.replace(regEx, '')
+  if (e2eTool === 'pw')
+    return html
+      .replace(/\s*<if-cy(?:\s+[\w-]+="[^"]*")*>[\s\S]*?<\/if-cy>/g, '')
+      .replace(/<\/?if-pw>/g, '')
+  else
+    return html
+      .replace(/\s*<if-pw(?:\s+[\w-]+="[^"]*")*>[\s\S]*?<\/if-pw>/g, '')
+      .replace(/<\/?if-cy>/g, '')
 }
 
 export function processLinkTags(html) {
   return html.replace(
-    /<app-link((?:\s+[\w-]+="[^"]*")*)>\s*([\s\S]*?)\s*<\/app-link>/g, 
+    /<app-link((?:\s+[\w-]+="[^"]*")*)>\s*([\s\S]*?)\s*<\/app-link>/g,
     '<a$1 href="$2">$2</a>'
   )
 }
 
 export function processLinks(html) {
-  return html.replace(
-    /<a((?:\s+[\w]+="[^"]*")*)>([\s\S]*?)<\/a>/g, 
-    (match, attributes, link) => {
-      if (attributes.indexOf('href="') !== -1) return match;
-      return '<a' + attributes + ' href="' + link + '">' + link + '</a>'
-    }
-  ).replace(/(<(?!a )[^<]*?>\s*)(https?:\/\/\S*?)([\s<])/g, '$1<a href="$2">$2</a>$3')
+  return html
+    .replace(
+      /<a((?:\s+[\w]+="[^"]*")*)>([\s\S]*?)<\/a>/g,
+      (match, attributes, link) => {
+        if (attributes.indexOf('href="') !== -1) return match
+        return '<a' + attributes + ' href="' + link + '">' + link + '</a>'
+      }
+    )
+    .replace(
+      /(<(?!a )[^<]*?>\s*)(https?:\/\/\S*?)([\s<])/g,
+      '$1<a href="$2">$2</a>$3'
+    )
 }
 
 export function processHelpTags(html) {
   return html.replace(
-    /<app-help(?:\s+class="([^"]*)")*>([\s\S]*?)<\/app-help>/g, 
+    /<app-help(?:\s+class="([^"]*)")*>([\s\S]*?)<\/app-help>/g,
     '<div class="admonition help $1"><p class="admonition__title">Help</p><div class="admonition__content">$2</div></div>'
   )
 }
 
 export function processExerciseTags(html) {
   return html.replace(
-    /<app-exercise(?:\s+class="([^"]*)")*>([\s\S]*?)<\/app-exercise>/g, 
+    /<app-exercise(?:\s+class="([^"]*)")*>([\s\S]*?)<\/app-exercise>/g,
     '<div class="exercice-2 $1"><div class="exercice__content">$2</div><img class="exercice__image"></div>'
   )
 }
